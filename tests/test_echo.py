28f9cd5b89509ccfaa37a7c3ec01cd091f0a6e77
Constantine Evans <constantine@costi.net>
2025-11-03 10:41:48 -0800

    short additional test

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -27,3 +27,30 @@ def test_echo_experiment():
     exp.add(m)
 
     p = exp.generate_picklist()
+
+def test_echo_fixed_volume():
+    pytest.importorskip('kithairon')
+    from riverine import EchoFixedVolume
+
+    # We'll make some components:
+    c1 = Component("c1", "10 µM", plate="plate1", well="A1")
+    c2 = Component("c2", "10 µM", plate="plate1", well="A2")
+    c3 = Component("c3", "5 µM", plate="plate1", well="A3")
+    c4 = Component("c4", "5 µM", plate="plate2", well="B3")
+
+    m = Mix(
+        [
+            EchoFixedVolume([c1, c2, c3, c4], "1 uL")
+        ],
+        "testmix", plate="destplate", well="A1"
+    )
+
+    mstr = str(m)
+
+    exp = Experiment()
+    exp.add(m)
+
+    p = exp.generate_picklist()
+
+    # All transfer volumes should be 1000
+    assert all(p.data["Transfer Volume"] == 1000)
