2ab0e1200a989e6bab4b9a7fe2319ef0faad88ba
Constantine Evans <constantine@costi.net>
2025-11-03 17:31:29 -0800

    more tests

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -5,7 +5,7 @@ from riverine import Component, Experiment, Mix
 
 def test_echo_experiment():
     pytest.importorskip('kithairon')
-    from riverine import EchoTargetConcentration
+    from riverine import EchoTargetConcentration, EchoFixedVolume, EchoFillToVolume
     exp = Experiment()
 
     # We'll make some components:
@@ -17,7 +17,34 @@ def test_echo_experiment():
 
     m = Mix(
         [
-            EchoTargetConcentration([c1, c2, c3, c4], "1 nM")
+            EchoTargetConcentration([c1, c2, c3, c4], "1 nM"),
+            EchoFillToVolume("Buffer", "100 uL"),
+        ],
+        "testmix", plate="destplate", well="A1"
+    )
+
+    mstr = str(m)
+
+    exp.add(m)
+
+    p = exp.generate_picklist()
+
+def test_echo_experiment_with_hand_fixed_volume():
+    pytest.importorskip('kithairon')
+    from riverine import EchoTargetConcentration, FillToVolume
+    exp = Experiment()
+
+    # We'll make some components:
+    c1 = Component("c1", "10 µM", plate="plate1", well="A1")
+    c2 = Component("c2", "10 µM", plate="plate1", well="A2")
+    c3 = Component("c3", "5 µM", plate="plate1", well="A3")
+    c4 = Component("c4", "5 µM", plate="plate2", well="B3")
+
+
+    m = Mix(
+        [
+            EchoTargetConcentration([c1, c2, c3, c4], "1 nM"),
+            FillToVolume("Buffer", "100 uL"),
         ],
         "testmix", plate="destplate", well="A1"
     )
